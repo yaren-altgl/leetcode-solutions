ec006d36008e1317528e4a0499ccf3bb731516eb
Yaren Altuğlu <yaren.altuglu@gmail.com>
2025-10-03 00:10:54 +0300

    Time: 634 ms (96.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+WITH first_year AS (
+  SELECT product_id, MIN(year) AS first_year
+  FROM Sales
+  GROUP BY product_id
+)
+SELECT 
+  s.product_id,
+  f.first_year,
+  s.quantity,
+  s.price
+FROM Sales s
+JOIN first_year f
+  ON s.product_id = f.product_id
+ AND s.year = f.first_year;
